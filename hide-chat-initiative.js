3cbd85e1e91f31458d2577b0a1d51a4866089f0e
Skepickle <skepickle@users.noreply.github.com>
2023-09-18 19:48:17 +0000

    Add support for FoundryVTT v11

--- hide-chat-initiative.js
+++ hide-chat-initiative.js
@@ -1,9 +1,14 @@
 Hooks.on('preCreateChatMessage', (doc, message, options, userid) => {
   if (message.flags !== undefined) {
-    //console.log(message);
+    if (message.flags.core !== undefined) {
+      if (message.flags.core.initiativeRoll) {
+        return false;
+      };
+    };
     if (message.flags['core.initiativeRoll']) {
-      //console.log("core.initiativeRoll was TRUE!");
       return false;
     };
   };
+  //console.log("Flags not found, here's the object passed in:");
+  //console.log(message);
 });
